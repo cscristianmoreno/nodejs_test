abc061f67fdb4ed569211e79b1e6f765c3088e70
cscristian <welcome_salta@hotmail.com>
2022-02-15 11:07:58 -0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { Router } = require("express");
 const express = require("express");
 
 const app = express();
 
 const port = proccess.env.PORT || 4000;
 
-app.listen(port);
-console.log(`teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest`);
+app.get("/", (req, res, next) => {
+    <h1>El puerto es `${port}`</h1>
+})
